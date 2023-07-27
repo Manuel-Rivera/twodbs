2b83d562f6e42d02d0b555ebedbc6b3e8f8c423d
Victor Manuel Rivera Pérez <1567079x@umich.mx>
2023-07-26 21:07:45 -0600

    Se concluyo la configuración de las bases de datos postgres y mysql

--- src/main/java/com/twodbs/demo/config/MySQLDB.java
+++ src/main/java/com/twodbs/demo/config/MySQLDB.java
@@ -2,16 +2,18 @@ package com.twodbs.demo.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.sql.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -28,9 +30,12 @@ transactionManagerRef = "mysqlTransactionManager",
 basePackages = {"com.twodbs.demo.repository.mysql"}
 )
 public class MySQLDB {
-    @Value("${mysql.jpa.database-platform}")
-    private String dialect;
-    
+    private final Environment env;
+
+    public MySQLDB(Environment env) {
+        this.env = env;
+    }
+
     @Primary
     @Bean(name = "mysqlDataSource")
     @ConfigurationProperties(prefix = "mysql.datasource")
@@ -41,13 +46,25 @@ public class MySQLDB {
     @Primary
     @Bean(name = "mysqlEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(@Qualifier("mysqlDataSource") DataSource dataSource){
+        //Add configuration session
+        HikariDataSource hirakiDataSource = new HikariDataSource();
+        hirakiDataSource.setMaximumPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("mysql.hikari.maximum-pool-size"))));
+        hirakiDataSource.setConnectionTimeout(Long.parseLong(Objects.requireNonNull(env.getProperty("mysql.hikari.connection-timeout"))));
+        hirakiDataSource.setIdleTimeout(Long.parseLong(Objects.requireNonNull(env.getProperty("mysql.hikari.idle-timeout"))));
+
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        factoryBean.setDataSource(hirakiDataSource);
         factoryBean.setDataSource(dataSource);
+
         factoryBean.setPackagesToScan("com.twodbs.demo.entity.mysql");
         HibernateJpaVendorAdapter vendor=new HibernateJpaVendorAdapter();
         factoryBean.setJpaVendorAdapter(vendor);
         Map<String,Object> properties = new HashMap<>();
-        properties.put("hibernate.dialect", dialect);
+        properties.put("hibernate.dialect", env.getProperty("mysql.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto",env.getProperty("mysql.hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql",env.getProperty("mysql.hibernate.show_sql"));
+        properties.put("hibernate.format_sql",env.getProperty("mysql.hibernate.format_sql"));
         factoryBean.setJpaPropertyMap(properties);
         return factoryBean;
     }
--- src/main/java/com/twodbs/demo/config/PostgresDB.java
+++ src/main/java/com/twodbs/demo/config/PostgresDB.java
@@ -1,15 +1,18 @@
 package com.twodbs.demo.config;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.sql.DataSource;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -22,13 +25,16 @@ import jakarta.persistence.EntityManagerFactory;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories( entityManagerFactoryRef = "postgresEntityManagerFactory",
-transactionManagerRef = "postgresTransactionManager",
-basePackages = {"com.twodbs.demo.repository.postgres"}
+        transactionManagerRef = "postgresTransactionManager",
+        basePackages = {"com.twodbs.demo.repository.postgres"}
 )
 public class PostgresDB {
-    @Value("${postgres.jpa.database-platform}")
-    private String dialect;
-    
+    private final Environment env;
+
+    public PostgresDB(Environment env) {
+        this.env = env;
+    }
+
     @Bean(name = "postgresDataSource")
     @ConfigurationProperties(prefix = "postgres.datasource")
     public DataSource dataSource(){
@@ -37,16 +43,29 @@ public class PostgresDB {
 
     @Bean(name = "postgresEntityManagerFactory")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(@Qualifier("postgresDataSource") DataSource dataSource){
+        //Add configuration session
+        HikariDataSource hirakiDataSource = new HikariDataSource();
+        hirakiDataSource.setMaximumPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("postgres.hikari.maximum-pool-size"))));
+        hirakiDataSource.setConnectionTimeout(Long.parseLong(Objects.requireNonNull(env.getProperty("postgres.hikari.connection-timeout"))));
+        hirakiDataSource.setIdleTimeout(Long.parseLong(Objects.requireNonNull(env.getProperty("postgres.hikari.idle-timeout"))));
+
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        factoryBean.setDataSource(hirakiDataSource);
         factoryBean.setDataSource(dataSource);
+
         factoryBean.setPackagesToScan("com.twodbs.demo.entity.postgres");
         HibernateJpaVendorAdapter vendor=new HibernateJpaVendorAdapter();
         factoryBean.setJpaVendorAdapter(vendor);
         Map<String,Object> properties = new HashMap<>();
-        properties.put("hibernate.dialect", dialect);
+        properties.put("hibernate.dialect", env.getProperty("postgres.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto",env.getProperty("postgres.hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.show_sql",env.getProperty("postgres.hibernate.show_sql"));
+        properties.put("hibernate.format_sql",env.getProperty("postgres.hibernate.format_sql"));
         factoryBean.setJpaPropertyMap(properties);
         return factoryBean;
     }
+    @Primary
     @Bean(name = "postgresTransactionManager")
     public PlatformTransactionManager platformTransactionManager(@Qualifier("postgresEntityManagerFactory") EntityManagerFactory entityManagerFactory){
         return new JpaTransactionManager(entityManagerFactory);
